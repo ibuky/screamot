5321afd003d7b380e16e4cbb8e26578c96fb75a8
ibuky@VTX0 <ivuki.maxx@gmail.com>
2020-06-09 17:05:42 +0900

    conform work

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,61 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+document.addEventListener('deviceready', function(event) {
+  console.log('initialize')
+})
+
+document.addEventListener('init', (event) => {
+  // Set click events
+  document.getElementById('button_ready1').addEventListener('click', this.onClickReady1, false)
+  document.getElementById('button_shout1').addEventListener('click', this.onClickShout1, false)
+  document.getElementById('button_ready2').addEventListener('click', this.onClickReady2, false)
+  document.getElementById('button_shout2').addEventListener('click', this.onClickShout2, false)
+})
+
+function onClickReady1 () {
+  onClickButton('standby1.jpg', null)
+  return
+}
+
+function onClickReady2 () {
+  onClickButton('standby2.jpg', null)
+  return
+}
+
+function onClickShout1 () {
+  onClickButton('sht1.jpg', 'sht1.mp3')
+  return
+}
+
+function onClickShout2 () {
+  onClickButton('sht2.jpg', 'sht2.mp3')
+  return
+}
+
+/**
+ * Set Button Click Event
+ * @param {String} imageName image file name to show
+ * @param {String} soundName sound file name to play
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+function onClickButton(imageName, soundName) {
+  if (!imageName) imageName = 'standby1'
+  if (!soundName) soundName = null
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+  // change image
+  const element = document.getElementById('marmot_image')
+  element.src = 'img/' + imageName
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+  // play sound if specified
+  if (!soundName) return
+  const media = new Media('/android_asset/www/audio/' + soundName, function() {
+    console.log('success')
+  }, function(err) {
+    console.log(err)
+  })
+  console.log(media)
+  media.play()
+  setTimeout(function () {
+    media.stop()
+    media.release()
+  }, 5000)
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
+  return
+}
